d04240fdc84e1a1469a8249a8d4d12fac4deb3ea
MkMula <maho.mamulashvili@gmail.com>
2022-03-25 09:29:48 -0400

    Add control flow statements
    '!done' exits the editor without saving. '!help' prints available syntax
     commands.

--- editor.py
+++ editor.py
@@ -1,10 +1,30 @@
 # write your code here
-print("""
-# John Lennon
-or ***John Winston Ono Lennon*** was one of *The Beatles*.
-Here are the songs he wrote I like the most:
-- Imagine
-- Norwegian Wood
-- Come Together
-- In My Life
-- ~~Hey Jude~~ (that was *McCartney*)""")
+
+FORMATTING_LST = ['plain', 'bold', 'italic', 'header', 'link',
+                  'inline-code', 'unordered-list',  'ordered-list', 'new-line']
+
+
+def available_formatters():
+    formatter = """Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line
+Special commands: !help !done"""
+
+    return formatter
+
+
+def main():
+
+    while True:
+        usr_inp = input('Chose a formatter: ')
+
+        if usr_inp == '!done':
+            break
+
+        if usr_inp == '!help':
+            print(available_formatters())
+
+        if usr_inp not in FORMATTING_LST and usr_inp not in ['!help', '!done']:
+            print('Unknown formatting type or command')
+
+
+if __name__ == '__main__':
+    main()
